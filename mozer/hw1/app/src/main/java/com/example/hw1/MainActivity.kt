ec960e5466de623fee725f4c6a77fe7b1595df98
Yana Mozer <yana.mozer99@gmail.com>
2022-03-17 18:55:10 +0700

    hw1 : implemented

--- mozer/hw1/app/src/main/java/com/example/hw1/MainActivity.kt
+++ mozer/hw1/app/src/main/java/com/example/hw1/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.hw1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val textInput = findViewById<EditText>(R.id.input).text
+            findViewById<TextView>(R.id.output).text = textInput
+            Toast.makeText(this, "You typed: $textInput", Toast.LENGTH_SHORT).show()
+        }
     }
 }
