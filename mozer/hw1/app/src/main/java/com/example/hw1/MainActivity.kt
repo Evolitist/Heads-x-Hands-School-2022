0b63d4f2cda56f5b865bd43a029a23d7f8fd9675
Yana Mozer <yana.mozer99@gmail.com>
2022-03-17 20:41:51 +0700

    hw1 : added viewBinding

--- mozer/hw1/app/src/main/java/com/example/hw1/MainActivity.kt
+++ mozer/hw1/app/src/main/java/com/example/hw1/MainActivity.kt
@@ -6,15 +6,20 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.example.hw1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<Button>(R.id.button).setOnClickListener {
-            val textInput = findViewById<EditText>(R.id.input).text
-            findViewById<TextView>(R.id.output).text = textInput
+        binding.button.setOnClickListener {
+            val textInput = binding.input.text
+            binding.output.text = textInput
             Toast.makeText(this, "You typed: $textInput", Toast.LENGTH_SHORT).show()
         }
     }
