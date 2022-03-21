c9ce83663c7214e73d4a4e7e45bbef9695573d7f
Yana Mozer <yana.mozer99@gmail.com>
2022-03-21 23:48:38 +0700

    hw2 : implemented

--- mozer/hw2/app/src/main/java/com/example/hw2/MainActivity.kt
+++ mozer/hw2/app/src/main/java/com/example/hw2/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.hw2
 
+import android.graphics.Color
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.text.toSpannable
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
     }
 }
