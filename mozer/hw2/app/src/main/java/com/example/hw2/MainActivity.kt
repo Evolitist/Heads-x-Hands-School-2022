a8ca92e02d2a70c2f3283a4aaa668470c833b4fa
Yana Mozer <yana.mozer99@gmail.com>
2022-03-24 18:32:46 +0700

    hw2 : implemented 100%

--- mozer/hw2/app/src/main/java/com/example/hw2/MainActivity.kt
+++ mozer/hw2/app/src/main/java/com/example/hw2/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.example.hw2
 
-import android.graphics.Color
-import android.graphics.Paint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.text.toSpannable
+import com.example.hw2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+
+
+
 
     }
 }
