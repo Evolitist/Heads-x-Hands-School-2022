7f114d9e9c105da73371973be1723fe9afe35663
Yana Mozer <yana.mozer99@gmail.com>
2022-03-26 19:34:35 +0700

    hw3 : implemented

--- mozer/hw3/app/src/main/java/com/example/hw3/MainActivity.kt
+++ mozer/hw3/app/src/main/java/com/example/hw3/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.hw3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.hw3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+
     }
 }
