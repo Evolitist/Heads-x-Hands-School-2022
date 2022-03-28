8ebee945da4740c9c7f97d3b49a83e3e413a5302
E. Patrusheva <elizaveta.patrusheva@gmail.com>
2022-03-28 23:45:32 +0700

    Add on button click action and toast

---
+++ patrusheva/lesson1/app/src/main/java/com/example/lesson1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.lesson1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.lesson1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            val typedText = binding.input.text
+            binding.output.text = typedText
+            Toast.makeText(this, "\"$typedText\" was typed", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
