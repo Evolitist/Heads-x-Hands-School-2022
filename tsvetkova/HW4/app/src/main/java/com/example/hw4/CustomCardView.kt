24012a464bd5f5cee2ce472195abb4f28faed1c5
InnaTsvetkova <mngr.tsvetkova@gmail.com>
2022-04-13 16:36:45 +0700

    Add homework 4

---
+++ tsvetkova/HW4/app/src/main/java/com/example/hw4/CustomCardView.kt
@@ -0,0 +1,28 @@
+package com.example.hw4
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.annotation.DrawableRes
+import androidx.cardview.widget.CardView
+import com.example.hw4.databinding.CardViewBinding
+
+//@JvmOverloads constructor
+class CustomCardView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defattrs: Int = 0
+) : FrameLayout(context, attrs, defattrs) {
+
+    private lateinit var binding: CardViewBinding
+
+    init {
+        binding =  CardViewBinding.inflate(LayoutInflater.from(context),this,true)
+    }
+
+
+    fun addImage(@DrawableRes id: Int) {
+        return binding.image.setImageResource(id)
+    }
+}
