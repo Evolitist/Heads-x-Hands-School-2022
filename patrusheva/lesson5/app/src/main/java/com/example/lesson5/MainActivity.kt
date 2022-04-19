8c93afe63bb329fdc98ae0690825c6b3f8407794
E. Patrusheva <elizaveta.patrusheva@gmail.com>
2022-04-19 20:27:34 +0700

    Fix setContentView

--- patrusheva/lesson5/app/src/main/java/com/example/lesson5/MainActivity.kt
+++ patrusheva/lesson5/app/src/main/java/com/example/lesson5/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         binding.recycler.adapter = postAdapter.apply {
             submitList(MockProvider.getMockPosts())
